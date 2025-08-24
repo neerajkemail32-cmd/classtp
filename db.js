028ad1824eb2cd9edbf5d3655c7e5e676915ae67
neeraj <neerajkemail32@gmail.com>
2025-08-24 09:40:20 +0530

    new

--- db.js
+++ db.js
@@ -1,7 +1,20 @@
+const mysql = require('mysql');
+
 const db = mysql.createConnection({
-  host: 'bo1ajjombryyob3pgecm-mysql.services.clever-cloud.com',   // Clever Cloud host
-  user: 'uvjszeaqytp22g2p',
-  password: 'uvjszeaqytp22g2p',
-  database: 'bo1ajjombryyob3pgecm',            // the DB name Clever Cloud gave you
-  port: 3306
+  host: process.env.DB_HOST,     
+  user: process.env.DB_USER,     
+  password: process.env.DB_PASS, 
+  database: process.env.DB_NAME, 
+  port: process.env.DB_PORT || 3306
 });
+
+db.connect((err) => {
+  if (err) {
+    console.error('❌ MySQL connection failed:', err.message);
+  } else {
+    console.log('✅ Connected to MySQL Database!');
+  }
+});
+
+
+module.exports = db;
